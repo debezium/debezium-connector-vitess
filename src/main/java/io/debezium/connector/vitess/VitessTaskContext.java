0f3aaf863e13931c9bdb1899a84e70e9ecdec90a
Shichao An <shichaoan.cn@gmail.com>
2021-12-23 10:54:00 -0800

    DBZ-4493: Add keyspace and shard properties in task context

--- src/main/java/io/debezium/connector/vitess/VitessTaskContext.java
+++ src/main/java/io/debezium/connector/vitess/VitessTaskContext.java
@@ -5,12 +5,28 @@
  */
 package io.debezium.connector.vitess;
 
+import java.util.Map;
+
 import io.debezium.connector.common.CdcSourceTaskContext;
+import io.debezium.util.Collect;
 
 /** A state (context) associated with a Vitess task. Used mostly by metrics collection. */
 public class VitessTaskContext extends CdcSourceTaskContext {
 
+    private final VitessConnectorConfig config;
+
     public VitessTaskContext(VitessConnectorConfig config, VitessDatabaseSchema schema) {
         super(config.getContextName(), config.getLogicalName(), schema::tableIds);
+        this.config = config;
+    }
+
+    @Override
+    public Map<String, String> getConnectorProperties() {
+        Map<String, String> props = Collect.hashMapOf("keyspace", config.getKeyspace());
+        String shard = config.getShard();
+        if (shard != null && !shard.isEmpty()) {
+            props.put("shard", shard);
+        }
+        return props;
     }
 }

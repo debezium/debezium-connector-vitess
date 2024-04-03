e973a4949757ba117c7d570b548993e8ff499729
twthorn <thomaswilliamthornton@gmail.com>
2024-04-03 12:37:17 -0500

    DBZ-7698 Override and use load instance method

--- src/test/java/io/debezium/connector/vitess/VitessOffsetContextTest.java
+++ src/test/java/io/debezium/connector/vitess/VitessOffsetContextTest.java
@@ -13,7 +13,11 @@ import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.debezium.connector.vitess.pipeline.txmetadata.VitessOrderedTransactionContext;
+import io.debezium.connector.vitess.pipeline.txmetadata.VitessTransactionInfo;
 import io.debezium.pipeline.txmetadata.TransactionContext;
 import io.debezium.util.Clock;
 import io.debezium.util.Collect;
@@ -100,16 +104,32 @@ public class VitessOffsetContextTest {
     }
 
     @Test
-    public void shouldLoadVitessOrderedTransactionContext() {
+    public void shouldLoadVitessOrderedTransactionContext() throws JsonProcessingException {
         VitessConnectorConfig config = new VitessConnectorConfig(
                 TestHelper.defaultConfig()
                         .with(VitessConnectorConfig.TRANSACTION_CONTEXT, VitessOrderedTransactionContext.class)
                         .build());
         VitessOffsetContext.Loader loader = new VitessOffsetContext.Loader(config);
-        Map offsets = Map.of(SourceInfo.VGTID_KEY, VGTID_JSON);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Long expectedEpoch1 = 2L;
+        Long expectedEpoch2 = 3L;
+        String shard1 = "-80";
+        String shard2 = "80-";
+        Map offsets = Map.of(
+                SourceInfo.VGTID_KEY, VGTID_JSON,
+                TransactionContext.OFFSET_TRANSACTION_ID, VGTID_JSON,
+                VitessOrderedTransactionContext.OFFSET_TRANSACTION_EPOCH, objectMapper.writeValueAsString(Map.of(
+                        shard1, expectedEpoch1,
+                        shard2, expectedEpoch2)));
         VitessOffsetContext context = loader.load(offsets);
         TransactionContext transactionContext = context.getTransactionContext();
         assertThat(transactionContext).isInstanceOf(VitessOrderedTransactionContext.class);
+        VitessOrderedTransactionContext orderedTransactionContext = (VitessOrderedTransactionContext) transactionContext;
+        assertThat(orderedTransactionContext.getPreviousVgtid()).isEqualTo(VGTID_JSON);
+        orderedTransactionContext.beginTransaction(new VitessTransactionInfo(VGTID_JSON, shard1));
+        assertThat(orderedTransactionContext.getTransactionEpoch()).isEqualTo(expectedEpoch1);
+        orderedTransactionContext.beginTransaction(new VitessTransactionInfo(VGTID_JSON, shard2));
+        assertThat(orderedTransactionContext.getTransactionEpoch()).isEqualTo(expectedEpoch2);
     }
 
     @Test
@@ -121,5 +141,7 @@ public class VitessOffsetContextTest {
         VitessOffsetContext context = VitessOffsetContext.initialContext(config, Clock.system());
         TransactionContext transactionContext = context.getTransactionContext();
         assertThat(transactionContext).isInstanceOf(VitessOrderedTransactionContext.class);
+        VitessOrderedTransactionContext orderedTransactionContext = (VitessOrderedTransactionContext) transactionContext;
+        assertThat(orderedTransactionContext.getPreviousVgtid()).isEqualTo(null);
     }
 }

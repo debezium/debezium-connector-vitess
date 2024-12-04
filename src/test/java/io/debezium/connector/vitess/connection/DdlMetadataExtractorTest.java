7552753822b06f296a722ba33dc7e6a9674994c5
twthorn <thomaswilliamthornton@gmail.com>
2024-12-04 15:03:03 -0500

    DBZ-8479 Handle all comment syntax

--- src/test/java/io/debezium/connector/vitess/connection/DdlMetadataExtractorTest.java
+++ src/test/java/io/debezium/connector/vitess/connection/DdlMetadataExtractorTest.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 
 import io.debezium.connector.vitess.TestHelper;
+import io.debezium.connector.vitess.VitessDatabaseSchema;
 import io.debezium.junit.logging.LogInterceptor;
 import io.debezium.schema.SchemaChangeEvent;
 
@@ -19,61 +20,87 @@ import io.debezium.schema.SchemaChangeEvent;
  */
 public class DdlMetadataExtractorTest {
 
+    private static String expectedTableName = VitessDatabaseSchema.buildTableId(
+            TestHelper.TEST_SHARD, TestHelper.TEST_UNSHARDED_KEYSPACE, TestHelper.TEST_TABLE).toDoubleQuotedString();
+
     @Test
     public void shouldGetAlterType() {
-        DdlMessage ddlMessage = new DdlMessage(null, null, "ALTER TABLE foo ADD COLUMN bar",
+        DdlMessage ddlMessage = new DdlMessage(null, null,
+                String.format("ALTER TABLE %s ADD COLUMN bar", TestHelper.TEST_TABLE),
                 TestHelper.TEST_UNSHARDED_KEYSPACE, TestHelper.TEST_SHARD);
         DdlMetadataExtractor extractor = new DdlMetadataExtractor(ddlMessage);
         assertThat(extractor.getSchemaChangeEventType()).isEqualTo(SchemaChangeEvent.SchemaChangeEventType.ALTER);
+        assertThat(extractor.getTable()).isEqualTo(expectedTableName);
     }
 
     @Test
     public void shouldGetCreateType() {
-        DdlMessage ddlMessage = new DdlMessage(null, null, "CREATE    TABLE foo",
+        DdlMessage ddlMessage = new DdlMessage(null, null,
+                String.format("CREATE    TABLE %s", TestHelper.TEST_TABLE),
                 TestHelper.TEST_UNSHARDED_KEYSPACE, TestHelper.TEST_SHARD);
         DdlMetadataExtractor extractor = new DdlMetadataExtractor(ddlMessage);
         assertThat(extractor.getSchemaChangeEventType()).isEqualTo(SchemaChangeEvent.SchemaChangeEventType.CREATE);
+        assertThat(extractor.getTable()).isEqualTo(expectedTableName);
     }
 
     @Test
     public void shouldGetTruncateType() {
-        DdlMessage ddlMessage = new DdlMessage(null, null, "TRUNCATE    TABLE foo",
+        DdlMessage ddlMessage = new DdlMessage(null, null,
+                String.format("TRUNCATE    TABLE %s", TestHelper.TEST_TABLE),
                 TestHelper.TEST_UNSHARDED_KEYSPACE, TestHelper.TEST_SHARD);
         DdlMetadataExtractor extractor = new DdlMetadataExtractor(ddlMessage);
         assertThat(extractor.getSchemaChangeEventType()).isEqualTo(SchemaChangeEvent.SchemaChangeEventType.TRUNCATE);
+        assertThat(extractor.getTable()).isEqualTo(expectedTableName);
     }
 
     @Test
     public void shouldGetTable() {
-        DdlMessage ddlMessage = new DdlMessage(null, null, "TRUNCATE    TABLE foo",
+        DdlMessage ddlMessage = new DdlMessage(null, null,
+                String.format("TRUNCATE    TABLE %s", TestHelper.TEST_TABLE),
                 TestHelper.TEST_UNSHARDED_KEYSPACE, TestHelper.TEST_SHARD);
         DdlMetadataExtractor extractor = new DdlMetadataExtractor(ddlMessage);
-        assertThat(extractor.getTable()).isEqualTo("\"0\".\"test_unsharded_keyspace\".\"foo\"");
+        assertThat(extractor.getTable()).isEqualTo(expectedTableName);
     }
 
     @Test
     public void shouldGetDropType() {
-        DdlMessage ddlMessage = new DdlMessage(null, null, "DROP TABLE foo",
+        DdlMessage ddlMessage = new DdlMessage(null, null,
+                String.format("DROP TABLE %s", TestHelper.TEST_TABLE),
                 TestHelper.TEST_UNSHARDED_KEYSPACE, TestHelper.TEST_SHARD);
         DdlMetadataExtractor extractor = new DdlMetadataExtractor(ddlMessage);
         assertThat(extractor.getSchemaChangeEventType()).isEqualTo(SchemaChangeEvent.SchemaChangeEventType.DROP);
+        assertThat(extractor.getTable()).isEqualTo(expectedTableName);
     }
 
     @Test
     public void shouldGetRenameType() {
-        DdlMessage ddlMessage = new DdlMessage(null, null, "RENAME TABLE foo TO bar",
+        DdlMessage ddlMessage = new DdlMessage(null, null,
+                String.format("RENAME TABLE %s TO %s_suffix", TestHelper.TEST_TABLE, TestHelper.TEST_TABLE),
                 TestHelper.TEST_UNSHARDED_KEYSPACE, TestHelper.TEST_SHARD);
         DdlMetadataExtractor extractor = new DdlMetadataExtractor(ddlMessage);
         assertThat(extractor.getSchemaChangeEventType()).isEqualTo(SchemaChangeEvent.SchemaChangeEventType.ALTER);
+        assertThat(extractor.getTable()).isEqualTo(expectedTableName);
     }
 
     @Test
     public void shouldParseStatementWithComments() {
-        DdlMessage ddlMessage = new DdlMessage(null, null, "rename /* gh-ost */ table `keyspace`.`table1` to " +
-                "`keyspace`.`_table1_del`, `keyspace`.`_table_gho` to `keyspace`.`table`",
+        DdlMessage ddlMessage = new DdlMessage(null, null,
+                String.format("rename /* gh-ost */ table `keyspace`.`%s` to `keyspace`.`_table1_del`, `keyspace`.`_table_gho` to `keyspace`.`table`",
+                        TestHelper.TEST_TABLE),
                 TestHelper.TEST_UNSHARDED_KEYSPACE, TestHelper.TEST_SHARD);
         DdlMetadataExtractor extractor = new DdlMetadataExtractor(ddlMessage);
         assertThat(extractor.getSchemaChangeEventType()).isEqualTo(SchemaChangeEvent.SchemaChangeEventType.ALTER);
+        assertThat(extractor.getTable()).isEqualTo(expectedTableName);
+    }
+
+    @Test
+    public void shouldParseStatementWithSingleLineComment() {
+        DdlMessage ddlMessage = new DdlMessage(null, null,
+                String.format("create \n # gh-ost \n table `keyspace`.`%s`;", TestHelper.TEST_TABLE),
+                TestHelper.TEST_UNSHARDED_KEYSPACE, TestHelper.TEST_SHARD);
+        DdlMetadataExtractor extractor = new DdlMetadataExtractor(ddlMessage);
+        assertThat(extractor.getSchemaChangeEventType()).isEqualTo(SchemaChangeEvent.SchemaChangeEventType.CREATE);
+        assertThat(extractor.getTable()).isEqualTo(expectedTableName);
     }
 
     @Test

86e784e42242e3630e402b6b9bda4b39e942fdef
twthorn <thomaswilliamthornton@gmail.com>
2024-12-19 16:46:56 -0700

    DBZ-8541 Handle partially overridden transactions

--- src/test/java/io/debezium/connector/vitess/pipeline/txmetadata/GtidTest.java
+++ src/test/java/io/debezium/connector/vitess/pipeline/txmetadata/GtidTest.java
@@ -17,37 +17,53 @@ import io.debezium.DebeziumException;
 
 public class GtidTest {
 
+    private static final String EXPECTED_VERSION = "MySQL56";
+    private static final String HOST_SET1 = "/host1:1,host2:2-10";
+    private static final String GTID1 = EXPECTED_VERSION + HOST_SET1;
+
     @Test
     public void shouldInit() {
-        String expectedVersion = "MySQL56";
-        Gtid gtid = new Gtid(expectedVersion + "/host1:1-4,host2:2-10");
-        assertThat(gtid.getVersion()).isEqualTo(expectedVersion);
+        Gtid gtid = new Gtid(EXPECTED_VERSION + "/host1:1-4,host2:2-10");
+        assertThat(gtid.getVersion()).isEqualTo(EXPECTED_VERSION);
         assertThat(gtid.getSequenceValues()).isEqualTo(List.of("4", "10"));
         assertThat(gtid.getHosts()).isEqualTo(Set.of("host1", "host2"));
     }
 
     @Test
     public void shouldHandleSingleValue() {
-        String expectedVersion = "MySQL56";
-        Gtid gtid = new Gtid(expectedVersion + "/host1:1,host2:2-10");
-        assertThat(gtid.getVersion()).isEqualTo(expectedVersion);
+        Gtid gtid = new Gtid(GTID1);
+        assertThat(gtid.getVersion()).isEqualTo(EXPECTED_VERSION);
         assertThat(gtid.getSequenceValues()).isEqualTo(List.of("1", "10"));
         assertThat(gtid.getHosts()).isEqualTo(Set.of("host1", "host2"));
     }
 
     @Test
+    public void testHostSupersetWithLargerSet() {
+        Gtid gtid = new Gtid(GTID1);
+        Gtid gtidSuperset = new Gtid(EXPECTED_VERSION + "/host1:1,host2:2-10,host3:1-5");
+        assertThat(gtidSuperset.isHostSetSupersetOf(gtid)).isTrue();
+        assertThat(gtid.isHostSetSupersetOf(gtidSuperset)).isFalse();
+    }
+
+    @Test
+    public void testHostSupersetWithEqualSet() {
+        Gtid gtid = new Gtid(GTID1);
+        Gtid gtid2 = new Gtid(GTID1);
+        assertThat(gtid.isHostSetSupersetOf(gtid2)).isTrue();
+        assertThat(gtid2.isHostSetSupersetOf(gtid)).isTrue();
+    }
+
+    @Test
     public void shouldThrowExceptionOnEmptyStringWithPrefix() {
-        String expectedVersion = "MySQL56";
         assertThatThrownBy(() -> {
-            Gtid gtid = new Gtid(expectedVersion + "/");
+            Gtid gtid = new Gtid(EXPECTED_VERSION + "/");
         }).isInstanceOf(DebeziumException.class);
     }
 
     @Test
     public void shouldThrowExceptionOnVersionOnly() {
-        String expectedVersion = "MySQL56";
         assertThatThrownBy(() -> {
-            Gtid gtid = new Gtid(expectedVersion);
+            Gtid gtid = new Gtid(EXPECTED_VERSION);
         }).isInstanceOf(DebeziumException.class);
     }
 
